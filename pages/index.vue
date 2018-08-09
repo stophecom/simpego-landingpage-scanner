f3605464c0d6061a44f78e03f6daa1172f98a909
kinchiller <kingchiller@gmail.com>
2018-08-09 23:36:48 +0200

    Minor styles

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,11 @@
     <ul class="manual">
       <li>
         <img alt="Scanner" src="/images/scan.svg" />
-        <div class="description">1. Fahrzeugausweis fotografieren</div>
+        <div class="description"><strong>1.</strong> Fahrzeugausweis fotografieren</div>
       </li>
       <li>
         <img alt="Done" src="/images/done.svg" />
-        <div class="description">2. Boom. Simpege liefert dir das beste Angebot.</div>
+        <div class="description"><strong><del>2.</del> Boom.</strong> Simpege liefert dir das beste Angebot.</div>
       </li>
     </ul>
     <div class="section__cta">
@@ -52,17 +52,21 @@ export default {
 
 <style lang="scss">
 .manual {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   margin: 0;
   padding: 0;
   text-align: center;
 
   @media screen and (min-width: $breakpointMini) {
-    display: flex;
+    align-items: flex-start;
+    flex-direction: row;
   }
 
   li {
-    padding: 2rem;
+    padding: 1.5rem;
     max-width: 250px;
 
     img {
