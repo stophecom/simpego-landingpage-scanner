eac46e0aa76ea4f486d882dd82f86faec45c2de4
kinchiller <kingchiller@gmail.com>
2018-08-10 23:03:31 +0200

    Add language switcher

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,84 +1,114 @@
 <template>
-    <footer class="footer">
-        <div class="container">
-            <nav>
-                <ul class="footer__menu">
-                    <li class="logo"><a class="active" aria-current="true" alt="Startseite" href="/"><img alt="Simpego Logo" src="/images/logo.svg" width="104" height="27"></a></li>
-                    <li><a aria-current="false" href="https://www.simpego.ch/about/">{{ $t('menu.about') }}</a></li>
-                    <li><a aria-current="false" href="https://www.simpego.ch/faq/">{{ $t('menu.faq') }}</a></li>
-                    <li><a aria-current="false" href="https://www.simpego.ch/rechtliches/">{{ $t('menu.legal') }}</a></li>
-                    <li><a aria-current="false" href="https://www.simpego.ch/kontakt/">{{ $t('menu.contact') }}</a></li>
-                </ul>
-            </nav>
-            <ul class="footer__trust-badges">
-                <li><a href="https://www.finma.ch" rel="noopener noreferrer" target="_blank"><img alt="Logo der Finanzmarktaufsicht (Finma)" src="/images/finma.png" width="107" height="28"></a></li>
-                <li><img alt="Diese Seite benutzt SSL-Verschlüsselung" src="/images/ssl-checkmark.png"></li>
-            </ul>
-        </div>
-        <ul class="social-media-links">
-            <li><a aria-label="Facebook" href="https://www.facebook.com/simpego" rel="noopener noreferrer" target="_blank"><span class="fa fa-facebook"></span></a></li>
-            <li><a aria-label="Twitter" href="https://twitter.com/Simpego_Schweiz" rel="noopener noreferrer" target="_blank"><span class="fa fa-twitter"></span></a></li>
-            <li><a aria-label="Instagram" href="https://www.instagram.com/simpego" rel="noopener noreferrer" target="_blank"><span class="fa fa-instagram"></span></a></li>
+  <footer class="footer">
+    <div class="container">
+      <nav>
+        <ul class="footer__menu">
+          <li class="logo">
+            <a class="active" aria-current="true" alt="Startseite" href="/">
+              <img alt="Simpego Logo" src="/images/logo.svg" width="104" height="27">
+            </a>
+          </li>
+          <li>
+            <a aria-current="false" href="https://www.simpego.ch/about/">{{ $t('menu.about') }}</a>
+          </li>
+          <li>
+            <a aria-current="false" href="https://www.simpego.ch/faq/">{{ $t('menu.faq') }}</a>
+          </li>
+          <li>
+            <a aria-current="false" href="https://www.simpego.ch/rechtliches/">{{ $t('menu.legal') }}</a>
+          </li>
+          <li>
+            <a aria-current="false" href="https://www.simpego.ch/kontakt/">{{ $t('menu.contact') }}</a>
+          </li>
         </ul>
-    </footer>
+      </nav>
+      <ul class="footer__trust-badges">
+        <li>
+          <a href="https://www.finma.ch" rel="noopener noreferrer" target="_blank">
+            <img alt="Logo der Finanzmarktaufsicht (Finma)" src="/images/finma.png" width="107" height="28">
+          </a>
+        </li>
+        <li>
+          <img alt="Diese Seite benutzt SSL-Verschlüsselung" src="/images/ssl-checkmark.png">
+        </li>
+      </ul>
+    </div>
+    <ul class="social-media-links">
+      <li>
+        <a aria-label="Facebook" href="https://www.facebook.com/simpego" rel="noopener noreferrer" target="_blank">
+          <span class="fa fa-facebook"></span>
+        </a>
+      </li>
+      <li>
+        <a aria-label="Twitter" href="https://twitter.com/Simpego_Schweiz" rel="noopener noreferrer" target="_blank">
+          <span class="fa fa-twitter"></span>
+        </a>
+      </li>
+      <li>
+        <a aria-label="Instagram" href="https://www.instagram.com/simpego" rel="noopener noreferrer" target="_blank">
+          <span class="fa fa-instagram"></span>
+        </a>
+      </li>
+    </ul>
+  </footer>
 </template>
 
 <style lang="scss">
-.footer {
-  background-color: white;
-  border-top: 1px solid $colorBorder;
-  padding: 1em $sectionPadding;
+  .footer {
+    background-color: white;
+    border-top: 1px solid $colorBorder;
+    padding: 1em $sectionPadding;
 
-  a {
-    color: $text-strong;
-    display: block;
-  }
-
-  .container {
-    align-items: flex-end;
-    display: flex;
-    margin-top: 2em;
-
-    @media screen and (min-width: $breakpointSmall) {
-      align-items: center;
+    a {
+      color: $text-strong;
+      display: block;
     }
-  }
 
-  &__menu {
-    @media screen and (min-width: $breakpointSmall) {
+    .container {
+      align-items: flex-end;
       display: flex;
+      margin-top: 2em;
+
+      @media screen and (min-width: $breakpointSmall) {
+        align-items: center;
+      }
     }
 
-    a {
-      padding: 0.5em;
+    &__menu {
+      @media screen and (min-width: $breakpointSmall) {
+        display: flex;
+      }
+
+      a {
+        padding: 0.5em;
+      }
+
+      .logo {
+        margin-right: 1em;
+      }
     }
 
-    .logo {
-      margin-right: 1em;
+    &__trust-badges {
+      display: flex;
+      margin-left: auto;
+
+      img {
+        object-fit: contain;
+        max-height: 1.2rem;
+      }
+    }
+
+    .social-media-links {
+      display: flex;
+      justify-content: center;
+      margin-top: 1em;
+
+      a {
+        font-size: 1.5rem;
+        opacity: 0.6;
+        padding: 0.4rem;
+      }
     }
   }
-
-  &__trust-badges {
-    display: flex;
-    margin-left: auto;
-
-    img {
-      object-fit: contain;
-      max-height: 1.2rem;
-    }
-  }
-
-  .social-media-links {
-    display: flex;
-    justify-content: center;
-    margin-top: 1em;
-
-    a {
-      font-size: 1.5rem;
-      opacity: 0.6;
-      padding: 0.4rem;
-    }
-  }
-}
 
 </style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,33 @@
 <template>
-    <nav class="header">
-        <div class="container header-inner">
-            <a class="logo" href="/">
-                      <img alt="Simpego Logo" src="/images/logo.svg" width="104" height="27">
-                    </a>
-            <span class="tagline">smart versichert</span>
-            <a class="link" aria-current="false" href="https://www.simpego.ch/kontakt/">Feedback</a>
-        </div>
-    </nav>
+  <nav class="header">
+    <div class="container header-inner">
+      <a class="logo" href="/">
+        <img alt="Simpego Logo" src="/images/logo.svg" width="104" height="27">
+      </a>
+      <span class="tagline">{{ $t('tagline') }}</span>
+      <i18n-switcher/>
+      <a class="link" aria-current="false" href="https://www.simpego.ch/kontakt/">Feedback</a>
+    </div>
+  </nav>
 </template>
 
+<script>
+  export default {
+    i18n: {
+      messages: {
+        en: {
+          tagline: 'smart insurance'
+        },
+        de: {
+          tagline: 'smart versichert'
+        }
+      }
+    }
+  }
+</script>
+
 <style lang="scss">
-.header {
+  .header {
     background-color: $defaultBackground;
     border-bottom: 1px solid $colorBorder;
     height: $header-height;
@@ -22,29 +38,49 @@
     right: 0;
     top: 0;
     z-index: 30;
-    .header-inner {
-        align-items: center;
-        display: flex;
-        height: 100%;
-    }
-    a {
-        padding: 0.5rem 0;
-    }
-    .logo {
-        display: flex;
-        align-items: center;
-    }
-    .tagline {
-        display: none;
-        font-size: 0.8rem;
-        margin-left: 1rem;
 
-        @media screen and (min-width: $breakpointMini) {
-          display: inline;
+    .header-inner {
+      align-items: center;
+      display: flex;
+      height: 100%;
+    }
+
+    .language-switcher {
+      display: flex;
+      margin-left: auto;
+      margin-right: 1em;
+      text-transform: uppercase;
+
+      > li {
+        &:not(:last-child)::after {
+          content: '|';
+          opacity: 0.3;
         }
+
+        a {
+          color: $text;
+          padding: 0.2rem;
+        }
+      }
     }
-    .link {
-        margin-left: auto;
+
+    a {
+      padding: 0.5rem 0;
     }
-}
+
+    .logo {
+      display: flex;
+      align-items: center;
+    }
+
+    .tagline {
+      display: none;
+      font-size: 0.8rem;
+      margin-left: 1rem;
+
+      @media screen and (min-width: $breakpointMini) {
+        display: inline;
+      }
+    }
+  }
 </style>
